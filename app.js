9a48cc024cffee5bb1c52970048389b8266c15ee
pranjalmahajan96 <pranjalmahajan96@gmail.com>
2020-12-08 13:32:06 +0530

    text area input

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!!!")
-})
+
+function clickEventHandler()
+{
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
 
 
 
